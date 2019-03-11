09e1788a4864908dc925b586dcdc7a51cd00807e
WildChang <43891937+WildChang@users.noreply.github.com>
2019-03-11 11:48:02 -0700

    mirrorVertical

--- pixlab/PictureTester.java
+++ pixlab/PictureTester.java
@@ -26,6 +26,23 @@ public class PictureTester
     caterpillar.explore();
   }
   
+   public static void testMirrorVerticalRightToLeft()
+  {
+    Picture caterpillar = new Picture("caterpillar.jpg");
+    caterpillar.explore();
+    caterpillar.mirrorVerticalRightToLeft();
+    caterpillar.explore();
+  }
+  
+  public static void testMirrorHorizontal()
+  {
+    Picture caterpillar = new Picture("caterpillar.jpg");
+    caterpillar.explore();
+    caterpillar.mirrorHorizontal();
+    caterpillar.explore();
+  }
+  
+  
   /** Method to test mirrorTemple */
   public static void testMirrorTemple()
   {
@@ -79,7 +96,8 @@ public class PictureTester
     //testNegate();
     //testGrayscale();
     //testFixUnderwater();
-    //testMirrorVertical();
+    testMirrorVerticalRightToLeft();
+    testMirrorHorizontal();
     //testMirrorTemple();
     //testMirrorArms();
     //testMirrorGull();
